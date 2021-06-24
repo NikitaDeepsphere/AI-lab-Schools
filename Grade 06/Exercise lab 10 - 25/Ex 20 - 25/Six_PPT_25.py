686030bdd1c40a2590fb6ce5b90f35555d88102d
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-06-24 21:57:40 +0530

    Update Six_PPT_25.py

--- Grade 06/Exercise lab 10 - 25/Ex 20 - 25/Six_PPT_25.py
+++ Grade 06/Exercise lab 10 - 25/Ex 20 - 25/Six_PPT_25.py
@@ -2,25 +2,27 @@
 Ex 25.
 
 Finally, Add all the values to make 1 long string with each number seperated by a space
+
+Program description: The objective of this program is to add all the values of a list containing different strings and make a large string.
 """
 
 int_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-for x in range(len(int_list)):
-  int_list[x]*=12
+for vAR_x in range(len(int_list)):
+  int_list[vAR_x]*=12
 
-add = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30]
-for x in range(len(int_list)):
-  int_list[x] = (int_list[x] + add[x])
+vAR_add = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30]
+for vAR_x in range(len(int_list)):
+  int_list[vAR_x] = (int_list[vAR_x] + vAR_add[vAR_x])
 
-total = 0
-for x in range (len(int_list)):
-  total+= int_list[x]
+vAR_total = 0
+for vAR_x in range (len(int_list)):
+  vAR_total+= int_list[vAR_x]
 
-for x in range(len(int_list)):
-  int_list[x] = str(int_list[x])
+for vAR_x in range(len(int_list)):
+  int_list[vAR_x] = str(int_list[vAR_x])
 
 print_me = ""
-for x in range(len(int_list)):
-  print_me = print_me + " " + int_list[x]
+for vAR_x in range(len(int_list)):
+  print_me = print_me + " " + int_list[vAR_x]
 print(print_me)

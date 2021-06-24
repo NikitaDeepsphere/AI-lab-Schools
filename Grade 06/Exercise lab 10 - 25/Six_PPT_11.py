75186b6602a36c4b525b6d7208408762cae2ce21
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-06-24 19:44:56 +0530

    Update Six_PPT_11.py

--- Grade 06/Exercise lab 10 - 25/Six_PPT_11.py
+++ Grade 06/Exercise lab 10 - 25/Six_PPT_11.py
@@ -4,7 +4,12 @@ Ex 11.
 Write a for loop to print out the counting numbers from 0 - 9
 
 Hint: for x in ___:
+Program description: The objective of this program is to print numbers from 0-9. This program highlights the usage of “for” loop in python. 
+This for loop is used to execute a set of statement, once for each item in a list. For loop is also used to write numbers.
+First the range is stored in the variable x and then it is printed.
+
+
 """
 
-for x in range(10):
-  print(x)
+for vAR_x in range(10):
+  print(vAR_x)

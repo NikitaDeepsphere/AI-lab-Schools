24c387ee8ae5a57797fa47d8e35007c0e01cd3bf
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-06-24 17:23:07 +0530

    Update Six_PPT_1.py

--- Grade 06/Exercise lab 1 - 10/Six_PPT_1.py
+++ Grade 06/Exercise lab 1 - 10/Six_PPT_1.py
@@ -1,7 +1,11 @@
 """
 Ex 1.
-
 Create Variable test with initial value 10
+
+Program description: The objective of this program is to assign a numeric value to a variable “vAR_test” with initial value 10.
+The equal sign (=) is used to assign value to variables in python.
+Note: We can assign two different types of value to a variable a numeric value as well a string value (text value).
+
 """
 
-test = 10
+vAR_test = 10

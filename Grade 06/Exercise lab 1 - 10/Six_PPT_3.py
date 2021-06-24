e829efd5b8f4419a4ce21c90f766d74764a667c7
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-06-24 17:32:05 +0530

    Update Six_PPT_3.py

--- Grade 06/Exercise lab 1 - 10/Six_PPT_3.py
+++ Grade 06/Exercise lab 1 - 10/Six_PPT_3.py
@@ -2,10 +2,14 @@
 Ex 3.
 
 Compare whether 2 conditional variables, day1 and day2 are equal
-
 Hint: Use ==
+
+Program description: The objective of this program is to compare whether two conditional variables “vAR_day1” and “vAR_day2” are equal and print it. 
+The values assigned for “vAR_day1” and “vAR_day2” are Monday and Wednesday respectively. This highlights the usage of == operator in python.
+Note: The == operator checks whether two variable point to the same data in the memory.
+
 """
 
-day1 = "Monday"
-day2 = "Wensday"
-print(day1 == day2)
+vAR_day1 = "Monday"
+vAR_day2 = "Wensday"
+print(vAR_day1 == vAR_day2)

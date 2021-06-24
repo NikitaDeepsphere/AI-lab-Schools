fc5d27f598ce8eb6748f7850d2df54d4c65683b5
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-06-24 17:37:30 +0530

    Update Six_PPT_4.py

--- Grade 06/Exercise lab 1 - 10/Six_PPT_4.py
+++ Grade 06/Exercise lab 1 - 10/Six_PPT_4.py
@@ -1,9 +1,14 @@
 """
 Ex 4.
 
-Create 2 variables, start_time and end_time and print the difference between the two
+Create 2 variables, vARstart_time and vARend_time and print the difference between the two
+Program description: The objective of this program is to print the difference between two variables “vARstart_time” and “vARend_time”. 
+The vARstart_time contains the numeric value 0 whereas the vARend_time contains the numeric value 15. The output printed will be the difference of “vARstart_time” and “vARend_time”. 
+This highlights the usage of print function in python.
+Note: The function of print statement can be used in several different ways.
+
 """
 
-start_time = 0
-end_time = 15
-print(end_time - start_time)
+vARstart_time = 0
+vARend_time = 15
+print(vARend_time - vARstart_time)

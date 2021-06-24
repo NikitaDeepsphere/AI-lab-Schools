9003cc54857734bc2dc03d47cf416f5e2737e367
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-06-24 20:36:24 +0530

    Update Six_PPT_15.py

--- Grade 06/Exercise lab 10 - 25/Six_PPT_15.py
+++ Grade 06/Exercise lab 10 - 25/Six_PPT_15.py
@@ -2,7 +2,13 @@
 Ex 15.
 
 Using the list created in ex. 14, print out the second slot of the list
+
+Program description: The objective of this program is to print out the second slot of the list named fruits containing values “Banana”, “Apple”, “Peach”. 
+The second slot of this list is Apple; Therefore, the output of this program is Apple.
+
+Note: A list can also have another list as an item. This is called a nested list.
+
 """
 
-fruits = ["Bannana", "Apple", "Peach"]
-print(fruits[1])
+vAR_fruits = ["Bannana", "Apple", "Peach"]
+print(vAR_fruits[1])

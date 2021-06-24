554b714d6d4777df5bab21cdd4de6d11d5cf901d
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-06-24 22:15:10 +0530

    Update Six_PPT_31.py

--- Grade 06/Exercise lab 26 - 40/Six_PPT_31.py
+++ Grade 06/Exercise lab 26 - 40/Six_PPT_31.py
@@ -1,17 +1,24 @@
 """
 Ex 31.
 
-Using user input, create a basic calculator that will add or subtract 2 numbers the user passes in. If the user passes in the numbers 1 , 2 and then types in -, then the output should be 1-2, or -1. If the user types in 3,4 and then +, the output should be 3+4, or 7.
+Using user input, create a basic calculator that will add or subtract 2 numbers the user passes in. If the user passes in the numbers 1 , 2 and then types in -,
+then the output should be 1-2, or -1. If the user types in 3,4 and then +, the output should be 3+4, or 7.
+
+Program description: The objective of this program is to get an input from the user of the program and perform a variety of arithmetic functions on it before returning the
+final answer. 
+This program imitates a 2 function calculator. Here the user can input the two numbers and their preferred arithmetic operation (addition or subtraction).
+Remember: You have to include a condition for even the worst case scenario when it comes to inputs, the desired format of input may not always be followed.
+
 """
 
-num1 = int(input("What is the first number:"))
-num2 = int(input("What is the second number:"))
-operation = input("What is the desired operation (- or +):")
+vAR_num1 = int(input("What is the first number:"))
+vAR_num2 = int(input("What is the second number:"))
+vAR_operation = input("What is the desired operation (- or +):")
 
 print() #This is just so there is a space
-if(operation == "-"):
-  print(num1 - num2)
-elif(operation == "+"):
-  print(num1+num2)
+if(vAR_operation == "-"):
+  print(vAR_num1 - vAR_num2)
+elif(vAR_operation == "+"):
+  print(vAR_num1+num2)
 else:
   print("Not Valid Operation Input")

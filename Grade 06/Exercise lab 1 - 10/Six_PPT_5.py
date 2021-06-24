e45f8039a3357823425b5ee220137a30296e7143
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-06-24 17:42:02 +0530

    Update Six_PPT_5.py

--- Grade 06/Exercise lab 1 - 10/Six_PPT_5.py
+++ Grade 06/Exercise lab 1 - 10/Six_PPT_5.py
@@ -1,14 +1,20 @@
 """
 Ex 5.
 
-Create 2 variables, time1 and time2 and print the greater variable
-
+Create 2 variables, vAR_time1 and vAR_time2 and print the greater variable
 Hint: Use if(): and else:
+
+Program description: The objective of this program is to create 2 variables “vAR_time1” and “vAR_time2” and print the variable containing the greater numeric value.
+“vAR_time1” and “vAR_time2” consists numeric values 10 and 15 respectively.
+This highlights the function of if and else statement in python. If statement is used for decision making operations.
+We can use else statement when we must justify one condition while the other condition is not true.
+Note: The else statement is an optional statement and there could only be one else statement following if.
+
 """
 
-time1 = 10
-time2 = 15
-if(time1 > time2):
-  print("time1")
+vAR_time1 = 10
+vAR_time2 = 15
+if(vAR_time1 > vAR_time2):
+  print("vAR_time1")
 else:
-  print("time2")
+  print("vAR_time2")

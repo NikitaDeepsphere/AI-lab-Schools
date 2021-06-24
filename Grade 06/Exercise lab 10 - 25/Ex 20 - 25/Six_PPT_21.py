17e85f3a7e28e7847238f20888c4de27e5b5a0a7
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-06-24 21:22:14 +0530

    Update Six_PPT_21.py

--- Grade 06/Exercise lab 10 - 25/Ex 20 - 25/Six_PPT_21.py
+++ Grade 06/Exercise lab 10 - 25/Ex 20 - 25/Six_PPT_21.py
@@ -2,8 +2,12 @@
 Ex 21.
 
 Multiply Each Number in the list by 12 (From Ex 20)
+
+Program description: The objective of this program is to multiply each number in the int_list by twelve.
+
 """
 
 int_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-for x in range(len(int_list)):
-  int_list[x]*=12
+for vAR_x in range(len(int_list)):
+  int_list[vAR_x]*=12
+  

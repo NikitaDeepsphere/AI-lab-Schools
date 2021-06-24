7d22736b47888aca6734a1047ce5e7bc3ed89c24
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-06-24 22:02:41 +0530

    Update Six_PPT_27.py

--- Grade 06/Exercise lab 26 - 40/Six_PPT_27.py
+++ Grade 06/Exercise lab 26 - 40/Six_PPT_27.py
@@ -2,11 +2,17 @@
 Ex 27.
 
 Grab the username and password through user input using input(). Print back the result.
+
+Program description: The objective of this program is to get an input from the user of the program and print it.
+This program specifically asks for the user’s username and password and then prints it out.
+Remember: The input function always takes user input as it is and converts it into a String.
+
+
 """
 
-username = input("Enter username:")
-password = input("Enter password:")
+vAR_username = input("Enter username:")
+vAR_password = input("Enter password:")
 
 print() #This is just so there is a space
-print("Username:",username)
-print("Password:",password)
+print("Username:",vAR_username)
+print("Password:",vAR_password)

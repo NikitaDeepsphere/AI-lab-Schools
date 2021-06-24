0869e5ff7f7abb9fd89424c17b06677196c38a2b
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-06-24 20:38:20 +0530

    Update Six_PPT_16.py

--- Grade 06/Exercise lab 10 - 25/Six_PPT_16.py
+++ Grade 06/Exercise lab 10 - 25/Six_PPT_16.py
@@ -2,7 +2,12 @@
 Ex 16.
 
 Using the list created in ex. 14, add (append) a new fruit, "Orange"
+
+Program description: The objective of this program is to add another fruit “orange” to an existing list named vAR_fruits.
+The append () in python adds a single item to the existing list.
+Note: After the method append is implemented the size of the list increases by one.
+
 """
 
-fruits = ["Bannana", "Apple", "Peach"]
-fruits.append("Orange")
+vAR_fruits = ["Bannana", "Apple", "Peach"]
+vAR_fruits.append("Orange")

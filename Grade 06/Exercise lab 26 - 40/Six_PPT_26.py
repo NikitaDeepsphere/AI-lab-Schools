201642fc974c4da2dcc638505a33822d6cfb25dc
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-06-24 21:59:53 +0530

    Update Six_PPT_26.py

--- Grade 06/Exercise lab 26 - 40/Six_PPT_26.py
+++ Grade 06/Exercise lab 26 - 40/Six_PPT_26.py
@@ -2,6 +2,13 @@
 Ex 26.
 
 Grab the username through user input using input()
+
+Program description: The objective of this program is to get input from the user of the program.
+It forms a basis for learning how to make your program interact with the users. 
+This program specifically asks for the user’s username.
+Remember: A prompt is a String that represents a default message before the input
+
+
 """
 
-username = input("Enter username:")
+vAR_username = input("Enter username:")

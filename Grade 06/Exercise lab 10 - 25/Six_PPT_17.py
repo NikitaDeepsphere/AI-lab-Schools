93e79f2bd52a46ff64ab199e312fda2bcb61d0eb
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-06-24 20:46:43 +0530

    Update Six_PPT_17.py

--- Grade 06/Exercise lab 10 - 25/Six_PPT_17.py
+++ Grade 06/Exercise lab 10 - 25/Six_PPT_17.py
@@ -4,7 +4,13 @@ Ex 17.
 Using the list created in ex. 14, print out each value in the list
 
 Hint: No loop required, just use print
+
+Program description: The objective of this program is to print all the values in the list (vAR_fruits). 
+“Print (vAR_fruits)” code prints the values in all slots.
+This program highlights the usage of print function in python.
+Note: Lists are used to store multiple value or items in a single variable.
+
 """
 
-fruits = ["Bannana", "Apple", "Peach"]
-print(fruits)
+vAR_fruits = ["Bannana", "Apple", "Peach"]
+print(vAR_fruits)

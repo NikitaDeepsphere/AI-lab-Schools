b801be0cf6b5ee44b5b93e334e121190b3e03fa7
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-06-24 21:34:14 +0530

    Update Six_PPT_23.py

--- Grade 06/Exercise lab 10 - 25/Ex 20 - 25/Six_PPT_23.py
+++ Grade 06/Exercise lab 10 - 25/Ex 20 - 25/Six_PPT_23.py
@@ -2,17 +2,19 @@
 Ex 23.
 
 Add Up all the values from each cell in the int_list (From Ex 22)
+Program description: The objective of this program is to add up all the value from each call in the int_list.
+
 """
 
 int_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-for x in range(len(int_list)):
-  int_list[x]*=12
+for vAR_x in range(len(int_list)):
+  int_list[vAR_x]*=12
 
-add = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30]
-for x in range(len(int_list)):
-  int_list[x] = (int_list[x] + add[x])
+vAR_add = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30]
+for vAR_x in range(len(int_list)):
+  int_list[vAR_x] = (int_list[vAR_x] + vAR_add[vAR_x])
 
-total = 0
-for x in range (len(int_list)):
-  total+= int_list[x]
+vAR_total = 0
+for vAR_x in range (len(int_list)):
+  vAR_total+= int_list[vAR_x]

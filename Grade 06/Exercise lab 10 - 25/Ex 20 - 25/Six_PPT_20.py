18781053fa6f8165eb4b9965cd4c613c694459a5
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-06-24 21:19:56 +0530

    Update Six_PPT_20.py

--- Grade 06/Exercise lab 10 - 25/Ex 20 - 25/Six_PPT_20.py
+++ Grade 06/Exercise lab 10 - 25/Ex 20 - 25/Six_PPT_20.py
@@ -2,6 +2,9 @@
 Ex 20.
 
 Create a list with 10 integer values assigned
+
+Program description: The objective of this program is to create a list with ten integer values. 
+The list vARint_list contains values 1,2,3,4,5,6,7,8,9,10.
 """
 
-int_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+vARint_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]

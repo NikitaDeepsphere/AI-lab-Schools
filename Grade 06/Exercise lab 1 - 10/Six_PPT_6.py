74d8cef011a9eaaa1f8e7ab85f9c232b2ef0e6bf
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-06-24 17:45:34 +0530

    Update Six_PPT_6.py

--- Grade 06/Exercise lab 1 - 10/Six_PPT_6.py
+++ Grade 06/Exercise lab 1 - 10/Six_PPT_6.py
@@ -2,11 +2,15 @@
 Ex 6.
 
 Create 2 variables, time1 and time2 and print the greater variable and its value
+Program description: The objective of this program is to create 2 variables “vAR_time1” and “vAR_time2” and print the variable containing the greater numeric value and its value.
+This is similar to exercise-5.
+
+
 """
 
-time1 = 10
-time2 = 15
-if(time1 > time2):
-  print("time1:", time1)
+vAR_time1 = 10
+vAR_time2 = 15
+if(vAR_time1 > vAR_time2):
+  print("vAR_time1:", vAR_time1)
 else:
-  print("time2:", time2)
+  print("vAR_time2:", vAR_time2)

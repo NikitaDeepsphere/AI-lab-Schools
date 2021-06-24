658361d6f79e5b6212cb2d6a47207f2bfa65de83
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-06-24 18:00:53 +0530

    Update Six_PPT_10.py

--- Grade 06/Exercise lab 1 - 10/Six_PPT_10.py
+++ Grade 06/Exercise lab 1 - 10/Six_PPT_10.py
@@ -1,8 +1,12 @@
 """
 Ex 10.
-
 Given a float, round the number and print it
+
+Program description: The objective of this program is round a float (decimal number) and print it. 
+This program highlights the function of Round () in python. 
+This will return you a float number that will be rounded to the decimal places which are given as input. 
+In this program the vARinput_num is 1411.911213.
 """
 
-input_num = 1411.911213
-print(round(input_num))
+vARinput_num = 1411.911213
+print(round(vARinput_num))

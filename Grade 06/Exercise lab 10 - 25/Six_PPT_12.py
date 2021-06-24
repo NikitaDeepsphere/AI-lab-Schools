d50eefd7f311456cb792218f7752fcacab037457
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-06-24 19:49:38 +0530

    Update Six_PPT_12.py

--- Grade 06/Exercise lab 10 - 25/Six_PPT_12.py
+++ Grade 06/Exercise lab 10 - 25/Six_PPT_12.py
@@ -3,8 +3,16 @@ Ex 12.
 
 Similar to previous question, but instead of 0 - 9, use 10 - 20
 
-Hint: Use for x in ___:
+Hint: Use for vAR_x in ___:
+
+Program description: The objective of this program is to print numbers Between 10 – 20. This program highlights the usage of “for” loop in python.
+In python “for” loop the first number is used to indicate the starting of the list or series. The second number is used to indicate the ending of the list or series. 
+So, the 10 -21is used to represent the starting number and the ending number. We might have a small doubt that the objective of this program is to print numbers 10 – 20 
+then why in the code it is written as 10 – 21. Python always prints 1 number less than we mention in the ending.
+Bonus point: The third number written in the “for” loop is to mention the difference between numbers (skip numbers).
+
+
 """
 
-for x in range(10,21):
-  print(x)
+for vAR_x in range(10,21):
+  print(vAR_x)

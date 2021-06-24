76a2136be1d1ebf042cbf8d25389493c1803cd6d
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-06-24 17:26:17 +0530

    Update Six_PPT_2.py

--- Grade 06/Exercise lab 1 - 10/Six_PPT_2.py
+++ Grade 06/Exercise lab 1 - 10/Six_PPT_2.py
@@ -2,7 +2,12 @@
 Ex 2.
 
 Create 2 variables start and end with the values "start" and "end" respectively
+
+Program description: The objective of this program is to assign string value (text value) to variables “vAR_start” and “vAR_end” with the values of start and end respectively.
+The equal sign (=) is used to assign value to variables in python.
+Note: The declaration automatically happens when you assign a numeric value or string value to a variable.
+
 """
 
-start = "start"
-end = "end"
+vAR_start = "start"
+vAR_end = "end"

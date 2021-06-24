a69a1e60bc1f46706886916279dadc46a3a5cf75
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-06-24 17:51:43 +0530

    Update Six_PPT_7.py

--- Grade 06/Exercise lab 1 - 10/Six_PPT_7.py
+++ Grade 06/Exercise lab 1 - 10/Six_PPT_7.py
@@ -2,10 +2,17 @@
 Ex 7.
 
 Given a float (decimal number), print how far the number is from 10.
+
+Program description: The objective of this program is to print how far the float (decimal number) is from 10. This program highlights the function of if statement and
+else statement. 
+Note: If statement contains a body of code which runs only when the condition given in the if statement is true the other way around, we use else statement only to justify 
+one condition while the other is not true.
+
+
 """
 
-input_num = -10.00
-if(input_num > 10):
-  print(input_num - 10)
+vARinput_num = -10.00
+if(vARinput_num > 10):
+  print(vARinput_num - 10)
 else:
-  print(10 - input_num)
+  print(10 - vARinput_num)

ca23c83aa34b88a7b70392f03ddb59558d02fb11
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-06-24 17:57:39 +0530

    Update Six_PPT_9.py

--- Grade 06/Exercise lab 1 - 10/Six_PPT_9.py
+++ Grade 06/Exercise lab 1 - 10/Six_PPT_9.py
@@ -2,7 +2,14 @@
 Ex 9.
 
 Print the remainder of a number when divided by 11
+
+Program description: The objective of this program is to print the remainder of a number when divided by 11. 
+This program highlights the % python modulo.
+Basically, this python modulo operation is used to get the remainder of a division. The vARinput_num in this program 1411. 
+Then, it prints the remainder after dividing.
+Note: The operands of python modulo operator can be either integer or float.
+
 """
 
-input_num = 1411
-print(input_num%11)
+vARinput_num = 1411
+print(vARinput_num%11)

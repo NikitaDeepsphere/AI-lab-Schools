00b2e60aaed602ba92b05ca7239ff0c7ca4c44cb
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-06-25 07:29:09 +0530

    Update Six_PPT_34.py

--- Grade 06/Exercise lab 26 - 40/Six_PPT_34.py
+++ Grade 06/Exercise lab 26 - 40/Six_PPT_34.py
@@ -2,7 +2,12 @@
 Ex 34.
 
 Create another function, subtract that does the opposite of add and subtracts the two numbers passed in.
+
+Program description: The objective of this program is to create a simple user-defined function that takes in 2 parameters and print out its difference.
+If you do not provide two numbers when calling the function, there will be an error.
+Notice: Isn’t this program and the program before this, similar to Ex. 31? Then why do we need functions?
+
 """
 
-def subtract(num1, num2):
-  print(num1 - num2)
+def subtract(vAR_num1, vAR_num2):
+  print(vAR_num1 - vAR_num2)

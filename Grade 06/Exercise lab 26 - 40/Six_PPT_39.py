9684fe2b8b5fd6bc99024cb182bfd1eb7a8919b7
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-06-25 08:06:01 +0530

    Update Six_PPT_39.py

--- Grade 06/Exercise lab 26 - 40/Six_PPT_39.py
+++ Grade 06/Exercise lab 26 - 40/Six_PPT_39.py
@@ -4,12 +4,11 @@ Ex 39.
 Create a function that sorts a list. Use the sorted() function which will sort a list alphaetically
 
 
-Program description: The objective of this program is to create a simple user-defined function that takes in an input from the user 
-of the program and performs a variety of arithmetic functions on it before returning the final answer. This program imitates a 4 
-function calculator. Here the user can input the two numbers and their preferred arithmetic operation (addition or subtraction).
-Remember: Unless a function is called, python will not perform the code under the specific function. Hence, function calls are very important. 
-Try to differentiate between arguments and parameters of a function.
-
+Program description: 
+Program description: The objective of this program is to create a function which uses the Python’s inbuilt functions.
+This program specifically uses sorted() functions to sort a list in the alphabetical order. It then returns the sorted list.
+Remember: Built-in functions are those that are already defined in Python libraries and we can call them directly.
+User defined functions are those that we define ourselves in our program using the ‘def’ keyword and then call them wherever we want.
 
 """
 

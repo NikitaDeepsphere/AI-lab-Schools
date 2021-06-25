062472ae09b6d5becaa0a3d7be4d16fcd9b08385
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-06-25 07:33:10 +0530

    Update Six_PPT_35.py

--- Grade 06/Exercise lab 26 - 40/Six_PPT_35.py
+++ Grade 06/Exercise lab 26 - 40/Six_PPT_35.py
@@ -4,8 +4,14 @@ Ex 35.
 Create a function that takes in a number and prints out all the numbers under it
 
 Hint: for x in ___
+
+Program description: The objective of this program is to create a simple user-defined function that combines both the knowledge of for loops and functions.
+This program specifically takes in a number and prints out all the numbers lesser than him.
+Remember: Indentation is extremely important in Python. Not indenting code that is supposed to come under a function properly will cause an error.
+
+
 """
 
-def under(num1):
-  for x in range(num1):
-    print(x)
+def under(vAR_num1):
+  for vAR_x in range(vAR_num1):
+    print(vAR_x)

309d1c34f2bf5f32b916e6c6dbd1d3b99e98c6dc
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-06-25 07:42:00 +0530

    Update Six_PPT_37.py

--- Grade 06/Exercise lab 26 - 40/Six_PPT_37.py
+++ Grade 06/Exercise lab 26 - 40/Six_PPT_37.py
@@ -2,10 +2,17 @@
 Ex 37.
 
 Create 2 functions where one multiplies by 5 and one takes the output of the first one and adds 4 to the output of it.
+
+Program description: The objective of this program is to create 2 user-defined functions.
+The function call of one function is placed within the other function.
+The first function ‘times5’ multiplies a given number by 5 and then returns it. 
+The second function calls ‘times5’ and then adds 4. 
+Remember: A function can return data as a result. 
+If there is no value that is returned by a function, it is called a void function.
+
 """
+def vAR_times5(vAR_num1):
+  return vAR_num1*5
 
-def times5(num1):
-  return num1*5
-
-def plus4(num1):
-  return times5(num1)+4
+def vAR_plus4(vAR_num1):
+  return vAR_times5(vAR_num1)+4

81182ce163472f4227bd1ff31d336ea947b18959
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-06-25 07:38:21 +0530

    Update Six_PPT_36.py

--- Grade 06/Exercise lab 26 - 40/Six_PPT_36.py
+++ Grade 06/Exercise lab 26 - 40/Six_PPT_36.py
@@ -1,7 +1,14 @@
 """
 Ex 36.
 
-Create a function similar to the previous that takes in a number and prints out the sum of all the numbers under it including the number itself. For example, sum_under(6) would print out 21.
+Create a function similar to the previous that takes in a number and prints out the sum of all the numbers under it including the number itself. 
+For example, sum_under(6) would print out 21.
+
+Program description: The objective of this program is to create a user-defined function that is similar to the one before, but instead prints out 
+the sum of the numbers less than the given number. 
+Try it out: What do you think will be the output when the number 1 is passed under this function. (Hint: try to be careful of the range)
+
+
 """
 
 def sum_under(num1):

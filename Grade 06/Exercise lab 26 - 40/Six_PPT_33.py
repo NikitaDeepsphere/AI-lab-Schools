525337d5968a8f14d021523d793f0121b61d2cf1
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-06-25 07:21:38 +0530

    Update Six_PPT_33.py

--- Grade 06/Exercise lab 26 - 40/Six_PPT_33.py
+++ Grade 06/Exercise lab 26 - 40/Six_PPT_33.py
@@ -2,7 +2,13 @@
 Ex 33.
 
 Adding on to the previous function, make it so that the function add, takes in 2 numbers and prints out their sum.
+
+Program description: The objective of this program is to create a simple user-defined function that takes in 2 parameters and prints out its sum.
+If you do not provide two numbers when calling the function, there will be an error.
+Remember: You can pass data, known as parameters, into a function.
+
+
 """
 
-def add(num1, num2):
-  print(num1 + num2)
+def add(vAR_num1, vAR_num2):
+  print(vAR_num1 + vAR_num2)

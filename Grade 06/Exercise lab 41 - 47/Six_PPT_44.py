7beef17fc503c1c27f99ed2a47dbf8f94dc84529
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-06-25 12:27:35 +0530

    Update Six_PPT_44.py

--- Grade 06/Exercise lab 41 - 47/Six_PPT_44.py
+++ Grade 06/Exercise lab 41 - 47/Six_PPT_44.py
@@ -2,17 +2,25 @@
 Ex 44.
 
 Given a list of integer numbers, find the range of the list (the maximum - minimum value)
+
+Program description: The objective of this program is to try and solve logical problems using code.
+The code specifically asks to return the range of a given list of numbers.
+You first have to try and get the maximum value, then the minimum value and then find and return the range.
+
+
+Hint: Range = Max value – Min value
+
 """
 
-numbers = [12,145,14,122,142,2,3154,132,132,242,1245,235,123] # can be any list, this is just an example
-def range(numbers):
-  min = numbers[0]
-  max = numbers[0] 
-  for x in numbers:
-    if(x < min):
-      min = x
-    if(x > max):
-      max = x
+vAR_numbers = [12,145,14,122,142,2,3154,132,132,242,1245,235,123] # can be any list, this is just an example
+def range(vAR_numbers):
+  min = vAR_numbers[0]
+  max = vAR_numbers[0] 
+  for vAR_x in vAR_numbers:
+    if(vAR_x < min):
+      min = vAR_x
+    if(vAR_x > max):
+      max = vAR_x
   return max-min
 
-print(range(numbers))
+print(range(vAR_numbers))

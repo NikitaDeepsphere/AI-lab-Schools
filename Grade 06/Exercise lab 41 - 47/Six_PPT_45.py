d855e0149652dde7e775b6336029b2b18254b103
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-06-25 12:32:16 +0530

    Update Six_PPT_45.py

--- Grade 06/Exercise lab 41 - 47/Six_PPT_45.py
+++ Grade 06/Exercise lab 41 - 47/Six_PPT_45.py
@@ -2,14 +2,21 @@
 Ex 45.
 
 Given a list of integer numbers, find the sum of all numbers that are either odd or less than 100
+
+
+Program description: The objective of this program is to try and solve logical problems using code. The code specifically asks to return the sum of numbers that are either odd or less than 100. 
+Hint: Try to write a Boolean statement which checks both conditions at the same time.
+
+
 """
 
-numbers = [12,145,14,122,142,2,3154,132,132,242,1245,235,123] # can be any list, this is just an example
-def odd_or_hundred(numbers):
-  total=0
-  for x in numbers:
-    if(x%2 == 1 or x<100):
-      total+=x
-  return total
 
-print(odd_or_hundred(numbers))
+vAR_numbers = [12,145,14,122,142,2,3154,132,132,242,1245,235,123] # can be any list, this is just an example
+def vAR_odd_orvAR_hundred(vAR_numbers):
+  vAR_total=0
+  for vAR_x in vAR_numbers:
+    if(vAR_x%2 == 1 or vAR_x<100):
+      vAR_total+=vAR_x
+  return vAR_total
+
+print(vAR_odd_orvAR_hundred(vAR_numbers))

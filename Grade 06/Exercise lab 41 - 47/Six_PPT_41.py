732f1dc1063fad1b7bac1e0df42d6b447382ba22
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-06-25 12:13:07 +0530

    Update Six_PPT_41.py

--- Grade 06/Exercise lab 41 - 47/Six_PPT_41.py
+++ Grade 06/Exercise lab 41 - 47/Six_PPT_41.py
@@ -2,12 +2,19 @@
 Ex 41.
 
 Given a list of integer numbers, return the amount of numbers in that list that are even.
+
+Program description: The objective of this program is to try and solve logical problems using code. 
+This code specifically returns the amount of numbers in a given list that are even.
+
+
+Hint: Use a loop and think about the conditions under which a number is even.
+
 """
 
-numbers = [1,2,3,4,5,6,7,8] # can be any list, this is just an example
-def count_evens(numbers):
+vAR_numbers = [1,2,3,4,5,6,7,8] # can be any list, this is just an example
+def count_evens(vAR_numbers):
   count = 0
-  for x in numbers:
-    if x % 2 == 0:
+  for vAR_x in numbers:
+    if vAR_x % 2 == 0:
       count+=1
   return count

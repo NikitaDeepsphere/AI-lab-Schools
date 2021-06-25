4b744ec82da83fed1e4adc6b870bc1d3f57938c9
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-06-25 08:00:41 +0530

    Update Six_PPT_39.py

--- Grade 06/Exercise lab 26 - 40/Six_PPT_39.py
+++ Grade 06/Exercise lab 26 - 40/Six_PPT_39.py
@@ -2,6 +2,15 @@
 Ex 39.
 
 Create a function that sorts a list. Use the sorted() function which will sort a list alphaetically
+
+
+Program description: The objective of this program is to create a simple user-defined function that takes in an input from the user 
+of the program and performs a variety of arithmetic functions on it before returning the final answer. This program imitates a 4 
+function calculator. Here the user can input the two numbers and their preferred arithmetic operation (addition or subtraction).
+Remember: Unless a function is called, python will not perform the code under the specific function. Hence, function calls are very important. 
+Try to differentiate between arguments and parameters of a function.
+
+
 """
 
 def list_sorter(sort_me):

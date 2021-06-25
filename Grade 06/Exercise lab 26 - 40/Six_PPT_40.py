685811d7a6721cb5dcd6680ae81ef09562aa51cd
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-06-25 08:07:38 +0530

    Update Six_PPT_40.py

--- Grade 06/Exercise lab 26 - 40/Six_PPT_40.py
+++ Grade 06/Exercise lab 26 - 40/Six_PPT_40.py
@@ -2,6 +2,13 @@
 Ex 40.
 
 Revisting the previous topics, create a 4 function calculator using functions that can add, subtract, multiply and divide. Take in user input like before.
+
+Program description: The objective of this program is to create a simple user-defined function that takes in an input from the
+user of the program and performs a variety of arithmetic functions on it before returning the final answer.
+This program imitates a 4 function calculator. Here the user can input the two numbers and their preferred arithmetic operation(addition or subtraction).
+Remember: Unless a function is called, python will not perform the code under the specific function. Hence, function calls are very important.
+Try to differentiate between arguments and parameters of a function.
+
 """
 
 num1 = int(input("What is the first number:"))

78d4ba03b1119606711d22e169fb74c14449c115
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-06-25 07:15:17 +0530

    Update Six_PPT_32.py

--- Grade 06/Exercise lab 26 - 40/Six_PPT_32.py
+++ Grade 06/Exercise lab 26 - 40/Six_PPT_32.py
@@ -2,6 +2,12 @@
 Ex 32.
 
 Now we are starting functions. Using the keyword def, create a basing function called add that does not take anything in, nor returns anything.
+
+Program description: The objective of this program is to start with an introduction to functions.
+You can learn how to create user-defined functions using the keyword ‘def’.
+The function does not take anything nor return anything. This program specifically prints out a blank line when called.
+Remember: A function is a block of code which only runs when it is called.
+
 """
 
 def add():

81d01157d3f8efa9cc793fd34c80619875e8f9c1
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-06-27 10:10:02 +0400

    Update Six_PPT_22.py

--- Grade 06/Exercise lab 10 - 25/Ex 20 - 25/Six_PPT_22.py
+++ Grade 06/Exercise lab 10 - 25/Ex 20 - 25/Six_PPT_22.py
@@ -3,14 +3,14 @@ Ex 22.
 
 Add Each value in the list (From Ex 21) to the corresponding value from: add = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30]
 
-Program description: The objective of this program is to add each value in the list “int_list” to the corresponding value [3, 6,9,12,15,18,21,24,27,30]
+Program description: The objective of this program is to add each value in the list “vARint_list” to the corresponding value [3, 6,9,12,15,18,21,24,27,30]
 The program highlights the usage of add function in python.
 
 """
 
-int_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-for vAR_x in range(len(int_list)):
-  int_list[vAR_x]*=12
+vARint_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for x in range(len(int_list)):
+  int_list[x]*=12
 vAR_add = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30]
-for vAR_x in range(len(int_list)):
-  int_list[vAR_x] = (int_list[vAR_x] + vAR_add[vAR_x])
+for x in range(len(int_list)):
+  int_list[x] = (int_list[x] + vAR_add[x])

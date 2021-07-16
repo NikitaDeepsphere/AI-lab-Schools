cdccab6dc4d47e7488d4d6c0abb4c8336eed22c7
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-07-16 21:26:39 +0530

    Update Seven_PPT_1.py

--- Grade 07/Exercise lab 01 - 15/Seven_PPT_1.py
+++ Grade 07/Exercise lab 01 - 15/Seven_PPT_1.py
@@ -3,6 +3,13 @@ Ex 1.
 
 Create 2 variables, time1 and time2
 
+Program description: The objective of this program is to assign a value to a variable, which is basically a memory location for storage of values of a datatype such as 
+(float, integer, string, character etc). 
+This program illustrates the declaration of variable” time 1” and “time 2 “that is creation of memory location and storage of numeric value 10 and 15 respectively.
+
+NOTE: WE CAN ASSIGN VALUE OF ANY DATA TYPE TO VARIABLE THAT IS FLOAT INTEGER STRING CHARACTER ETC.
+
+
 """
-time1 = 10
-time2 = 15
+vAR_time1 = 10
+vAR_time2 = 15

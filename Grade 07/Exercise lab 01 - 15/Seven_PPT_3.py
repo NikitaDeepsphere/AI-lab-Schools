37f99e78efb1cad3acc2968220929e21c8fc78a1
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-07-17 13:27:07 +0530

    Update Seven_PPT_3.py

--- Grade 07/Exercise lab 01 - 15/Seven_PPT_3.py
+++ Grade 07/Exercise lab 01 - 15/Seven_PPT_3.py
@@ -4,10 +4,16 @@ Ex 3.
 Print whether a number is even or odd
 
 Hint: Use %
+
+Program description: In this program we will be declaring a variable” input_num ” and initializing integer data value of 141 
+using the looping statement ifelse, we are going to check if this number is odd or even. if “input_num”mod 2 ==0 then print 
+even or print the statement in else statement.
+NOTE: % IS USED FOR FINDING REMAINDERS
+
 """
 
-input_num = 141
-if(input_num%2 == 0):
+vAR_input_num = 141
+if(vAR_input_num%2 == 0):
   print("even")
 else:
   print("odd")

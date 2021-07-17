921056e24cf5f68db234a5f0864ef875663467a7
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-07-17 17:30:55 +0530

    Update Seven_PPT_29.py

--- Grade 07/Exercise lab 26 - 40/Seven_PPT_29.py
+++ Grade 07/Exercise lab 26 - 40/Seven_PPT_29.py
@@ -2,17 +2,23 @@
 Ex 29.
 
 Add to the previous class another function, get_type that will print the value of animal type
+
+Program description: The objective of this program is to add another to the previous class another function,
+get_type which will print the value of an animal type.
+
+Remember: The normal way to add functionality to a class in Python is to define functions in the class body.
+
 """
 
 class animal:
    animal_type = ""
-   age = 0
+   vAR_age = 0
 
-   def change_type(self,str_in):
-     self.animal_type = str_in
+   def vAR_change_type(self,vAR_str_in):
+     self.animal_type = vAR_str_in
 
-   def change_age(self,int_in):
-      self.age = int(int_in)
+   def vAR_change_age(self,vAR_int_in):
+      self.age = int(vAR_int_in)
    
    def get_type(self):
      print(self.animal_type)

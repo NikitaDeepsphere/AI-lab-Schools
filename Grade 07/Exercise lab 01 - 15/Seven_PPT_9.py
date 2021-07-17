0a2b52224c64d55ef485cfb9494b6121b6911f23
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-07-17 13:41:25 +0530

    Update Seven_PPT_9.py

--- Grade 07/Exercise lab 01 - 15/Seven_PPT_9.py
+++ Grade 07/Exercise lab 01 - 15/Seven_PPT_9.py
@@ -1,11 +1,15 @@
 """
 Ex 9.
 
-Using the list created in ex. 6, given the list, new_fruits = ["Pineapple", "Watermelon"], replace the corresponding value of fruits, with the corresponding values of new_fruits
+Using the list created in ex. 6, given the list, new_fruits = ["Pineapple", "Watermelon"], replace the corresponding value of fruits, 
+with the corresponding values of new_fruits
+
+Program description: this program deals with changing the corresponding value of one list with that of other list that is 
+changing the corresponding value of variable “fruits” with “new_fruits”len()function is used to find the length of list.
 """
 
-fruits = ["Bannana", "Apple", "Peach"]
-new_fruits = ["Pineapple", "Watermelon"]
-for x in range(len(new_fruits)):
-  fruits[x] = new_fruits[x]
-print(fruits)
+vAR_fruits = ["Bannana", "Apple", "Peach"]
+vAR_new_fruits = ["Pineapple", "Watermelon"]
+for vAR_x in range(len(vAR_new_fruits)):
+  vAR_fruits[vAR_x] = vAR_new_fruits[vAR_x]
+print(vAR_fruits)

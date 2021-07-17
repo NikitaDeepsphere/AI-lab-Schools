370dfe081defcdcd4489cdc77e08abc974ba773a
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-07-17 17:10:15 +0530

    Update Seven_PPT_22.py

--- Grade 07/Exercise lab 21 - 25/Seven_PPT_22.py
+++ Grade 07/Exercise lab 21 - 25/Seven_PPT_22.py
@@ -2,10 +2,17 @@
 Ex 22.
 
 Create a function that will return the first half of a string (can be a word or sentence)
+
+Program description: The objective of this program is to create a function that will return the first half of a string this string can be either word or sentence.
+For example: -
+                  *” Half of this sentence is “=  Half of this
+                  *Hi= H
+
 """
 
-str1 = "Half of this sentence is"
+
+vAR_str1 = "Half of this sentence is"
 def half(string):
   return string[:int(len(string)/2)]
 
-print(half(str1))
+print(half(vAR_str1))

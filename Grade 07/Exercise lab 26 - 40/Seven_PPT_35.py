be00d782997ab079fd171a32c4596e0d2b180e35
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-07-17 18:02:51 +0530

    Update Seven_PPT_35.py

--- Grade 07/Exercise lab 26 - 40/Seven_PPT_35.py
+++ Grade 07/Exercise lab 26 - 40/Seven_PPT_35.py
@@ -1,39 +1,44 @@
 """
 Ex 35.
 
-Add to the Calculator class a calculate function that depending on the operation, will call one of the other functions and return the output. Ex. if operation is "*", then it will return self.multiply()
+Add to the Calculator class a calculate function that depending on the operation, will call one of the other functions and return the output.
+Ex. if operation is "*", then it will return self.multiply()
+
+Program description: The objective of this program is to add a function to the Calculator Class which will call one of the other functions and return the output.
+It uses a set of if and else statements to test the operation attribute against “*”,”/”, “+”, “-“ and decide which operation to use.
+Additionally, a line to test out the Calculator Class functions is also provided.
 """
 
 class Calculator:
-   num1 = 0
-   num2 = 0
-   operation = ""
+   vAR_num1 = 0
+   vAR_num2 = 0
+   vAR_operation = ""
 
-   def __init__ (self, num1, num2, operation):
-     self.num1 = int(num1)
-     self.num2 = int(num2)
-     self.operation = operation
+   def __init__ (self, vAR_num1, vAR_num2, vAR_operation):
+     self.vAR_num1 = int(vAR_num1)
+     self.vAR_num2 = int(vAR_num2)
+     self.vAR_operation = vAR_operation
 
    def add(self):
-     return self.num1+self.num2
+     return self.vAR_num1+self.vAR_num2
 
    def subtract(self):
-     return self.num1-self.num2
+     return self.vAR_num1-self.vAR_num2
 
    def multiply(self):
-     return self.num1*self.num2
+     return self.vAR_num1*self.vAR_num2
 
    def divide(self):
-     return self.num1/self.num2
+     return self.vAR_num1/self.vAR_num2
    
    def calculate(self):
-     if(self.operation == "*"):
+     if(self.vAR_operation == "*"):
        return self.multiply()
 
-     elif(self.operation == "/"):
+     elif(self.vAR_operation == "/"):
        return self.divide()
      
-     elif(self.operation == "-"):
+     elif(self.vAR_operation == "-"):
        return self.subtract()
 
      else:

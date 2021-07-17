c804b57356a30ea1d88c5ce4cc967cb9ebcad9f2
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-07-17 17:13:23 +0530

    Update Seven_PPT_23.py

--- Grade 07/Exercise lab 21 - 25/Seven_PPT_23.py
+++ Grade 07/Exercise lab 21 - 25/Seven_PPT_23.py
@@ -2,12 +2,19 @@
 Ex 23.
 
 Create a function that will pass in 2 strings and will output the second string inserted halfway in the first. Example: insert("Apple", "Orange") = "ApOrangeple"
+
+Program description: The objective of this program is to create a function that will pass in two strings and will output the second string inserted halfway in the first.
+For example:-
+                    *Apple and Orange = Aporangeple
+                     * Pear and lemon = PeLemonar
+Note : - The first word is splitted in two and the second word is added in between the splitted words
+
 """
 
-str1 = "Apple"
-str2 = "Orange"
+vAR_str1 = "Apple"
+vAR_str2 = "Orange"
 
-def insert(str1, str2):
-  return str1[:int(len(str1)/2)] + str2 + str1[int(len(str1)/2):]
+def insert(vAR_str1, vAR_str2):
+  return vAR_str1[:int(len(vAR_str1)/2)] + vAR_str2 + vAR_str1[int(len(vAR_str1)/2):]
 
-print(insert(str1,str2))
+print(insert(vAR_str1,vAR_str2))

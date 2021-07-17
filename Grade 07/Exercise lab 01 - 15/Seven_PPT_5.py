19ef4fc3e7ba2729ea603d0b3253d694511a2b77
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-07-17 13:32:37 +0530

    Update Seven_PPT_5.py

--- Grade 07/Exercise lab 01 - 15/Seven_PPT_5.py
+++ Grade 07/Exercise lab 01 - 15/Seven_PPT_5.py
@@ -2,11 +2,18 @@
 Ex 5.
 
 Similar to past question, but write a while loop to print out the counting numbers from 10-20
-"""
 
-x = 10
-while x < 21:
-  print(x)
-  x+=1    
+Program description: In this program we are going to print the numbers in the range using while statements. 
+Here we declare a variable ‘x’and initialize it as 10 and give it with a while condition that the value 
+should be lesser than21 and we also increment which is increasing the value of x by 1, each time the loop runs. 
+And we print so each time the loop runs it will increase its value by 1 and check with e condition and if the
+condition satisfies its proceeds or if not, the loops terminates.
+NOTE: THE VALUE CAN ALSO DECREMENT BY USING THE CONDITION X=X-1
+
+"""
+vAR_x = 10
+while vAR_x < 21:
+  print(vAR_x)
+  vAR_x+=1    
   
-  #Or x = x+1 also works
+  #Or vAR_x = vAR_x+1 also works

3ce37d362b10b5ba1401dd913cfcbb8ff15525b5
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-07-17 14:09:13 +0530

    Update Seven_PPT_17.py

--- Grade 07/Exercise lab 16 - 20/Seven_PPT_17.py
+++ Grade 07/Exercise lab 16 - 20/Seven_PPT_17.py
@@ -1,14 +1,21 @@
 """
 Ex 17.
 
-Create a function that will combine a list of strings and ouput a sentence of the strings in the list. Example: if list = ["This", "is", "a", "test"], then the output would be "This is a test"
+Create a function that will combine a list of strings and ouput a sentence of the strings in the list. Example: if list = ["This", "is", "a", "test"], 
+then the output would be "This is a test"
+
+Program description: Program description: The objective of this program is to create a function that combines a list of strings in the list. 
+For example: List = [“This ”, “ is ”, “ a ”,” test ”,] = This is a test
+
+Note: Each string in the list would be equal to each word in the sentence.
+
 """
 
-check = ["This", "is", "a", "test"]
-def sentence(check):
-  output = ""
-  for x in check:
-    output = output + x + " "
-  return output
+vAR_check = ["This", "is", "a", "test"]
+def vAR_sentence(vAR_check):
+  vAR_output = ""
+  for vAR_x in vAR_check:
+    vAR_output = vAR_output + vAR_x + " "
+  return vAR_output
 
-print(sentence(check))
+print(vAR_sentence(vAR_check))

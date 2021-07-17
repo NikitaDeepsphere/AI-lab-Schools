7a2b8550c5036b5da40abef259e000b5775af1ae
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-07-17 17:27:00 +0530

    Update Seven_PPT_28.py

--- Grade 07/Exercise lab 26 - 40/Seven_PPT_28.py
+++ Grade 07/Exercise lab 26 - 40/Seven_PPT_28.py
@@ -2,14 +2,21 @@
 Ex 28.
 
 Add to the preivous class another variable age and a corresponding function, change_age to change the age.
+
+Program description: The objective of this program is to add to the previous class another variable “age” and a corresponding function and change_age to change the age.
+Answer time:
+ Now after reading the last 3 exercises some of you might have a doubt that for what is this class used for?
+Answer: - Class creates a user-defined data structure, which holds its own data members and members functions, 
+which can be accessed and used by creating an instance of that class. A class is an outline, copy or blueprint of that object.
+
 """
 
 class animal:
   animal_type = ""
-  age = 0
+  vAR_age = 0
 
-  def change_type(self,str_in):
-    self.animal_type = str_in
+  def change_type(self,vAR_str_in):
+    self.animal_type = vAR_str_in
 
-  def change_age(self,int_in):
-    self.age = int(int_in)
+  def change_age(self,vAR_int_in):
+    self.vAR_age = int(vAR_int_in)

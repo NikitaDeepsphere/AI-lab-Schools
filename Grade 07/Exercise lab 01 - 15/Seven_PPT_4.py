9d3e17525459157714816f074ac426351d78b4bf
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-07-17 13:29:30 +0530

    Update Seven_PPT_4.py

--- Grade 07/Exercise lab 01 - 15/Seven_PPT_4.py
+++ Grade 07/Exercise lab 01 - 15/Seven_PPT_4.py
@@ -4,7 +4,14 @@ Ex 4.
 Write a for loop to print out the counting numbers from 10 - 20
 
 Hint: for x in ___:
+
+Program description: in this program we need to print the numbers in range, so in such case we use for statement of that particular
+variable to print the number present In the range.
+
+So, for x in range (10,21) signifies range between 10-21 and print statement prints those numbers.
+NOTE: NOT NESSACARY TO USE ONLY FOR STATEMENTS YOU CAN ALSO USE OTHER CONITIONAL STATEMENTS SUCH AS DOWHILE AND WHILE STATEMENTS.
+
 """
 
-for x in range(10,21):
-  print(x)
+for vAR_x in range(10,21):
+  print(vAR_x)

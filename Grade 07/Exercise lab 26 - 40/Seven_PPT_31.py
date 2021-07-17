0b6eb4d64592f14dee48683429b32dd4af968fba
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-07-17 17:46:23 +0530

    Update Seven_PPT_31.py

--- Grade 07/Exercise lab 26 - 40/Seven_PPT_31.py
+++ Grade 07/Exercise lab 26 - 40/Seven_PPT_31.py
@@ -2,9 +2,11 @@
 Ex 31.
 
 Create a new class Calculator which has the variables num1 , num2 , and operation
+
+Program description: The objective of this program is to create a new class named “Calculator” which has the variables “num1” and “num2” and the operation.
 """
 
 class Calculator:
-   num1 = 0
-   num2 = 0
-   operation = ""
+   vAR_num1 = 0
+   vAR_num2 = 0
+   vAR_operation = ""

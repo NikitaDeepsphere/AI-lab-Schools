969dad0710b9abeb9f74284e55637c01cadc30e4
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-07-17 13:35:00 +0530

    Update Seven_PPT_6.py

--- Grade 07/Exercise lab 01 - 15/Seven_PPT_6.py
+++ Grade 07/Exercise lab 01 - 15/Seven_PPT_6.py
@@ -2,6 +2,9 @@
 Ex 6.
 
 Create a list called fruits that stores the values "Bannana", "Apple", and "Peach"
+
+Program description: in this program we will be creating a list which is a collection of items,so in this program we will be creating  
+ list of “fruits "and adding the list of fruits using square brackets and separated by comma with double quotation as it is a string.
 """
 
-fruits = ["Bannana", "Apple", "Peach"]
+vAR_fruits = ["Bannana", "Apple", "Peach"]

19151ea87d25749ab11f109314a2b82563bb6d63
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-07-17 13:56:17 +0530

    Update Seven_PPT_14.py

--- Grade 07/Exercise lab 01 - 15/Seven_PPT_14.py
+++ Grade 07/Exercise lab 01 - 15/Seven_PPT_14.py
@@ -2,12 +2,18 @@
 Ex 14.
 
 Given a list of numbers, if anywhere in the list, there are 2 consecutive numbers (list[x] == list[x+1]), return true, else return false.
+
+Program description: def is the keyword for defining a function.
+The function name is followed by parameters in (). 
+The colon signals the start of the function body, which is marked by indentation. 
+Inside the function body, the return statement determines the value to be returned. 
+Numbers is a list defining a function
 """
-numbers = [12,145,14,122,142,2,3154,132,132,242,1245,235,123] # can be any list, this is just an example
-def consecutive(numbers):
-  for x in range(len(numbers)-1):
-    if(numbers[x] == numbers[x+1]):
+vAR_numbers = [12,145,14,122,142,2,3154,132,132,242,1245,235,123] # can be any list, this is just an example
+def vAR_consecutive(vAR_numbers):
+  for vAR_x in range(len(vAR_numbers)-1):
+    if(vAR_numbers[vAR_x] == vAR_numbers[vAR_x+1]):
       return True
   return False
 
-print(consecutive(numbers))
+print(vAR_consecutive(vAR_numbers))

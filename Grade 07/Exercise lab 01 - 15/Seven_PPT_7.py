16f9edf8432814ac23b7e5ad252f710f4714fa56
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-07-17 13:36:35 +0530

    Update Seven_PPT_7.py

--- Grade 07/Exercise lab 01 - 15/Seven_PPT_7.py
+++ Grade 07/Exercise lab 01 - 15/Seven_PPT_7.py
@@ -2,7 +2,10 @@
 Ex 7.
 
 Using the list created in ex. 6, print out the second slot of the list
+
+Program description: using the above list we need t print the second slot here we assume thst the first slot 
+is [0] will second slot is [1] and so on so for eg ;38 slot will be [37].we will use syntax as print(list name[slot number])
 """
 
-fruits = ["Bannana", "Apple", "Peach"]
-print(fruits[1])
+vAR_fruits = ["Bannana", "Apple", "Peach"]
+print(vAR_fruits[1])

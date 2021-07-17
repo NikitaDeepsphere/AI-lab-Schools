9843126b4743ae5afe745524e0bbf08ea4c82e33
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-07-17 13:24:57 +0530

    Update Seven_PPT_2.py

--- Grade 07/Exercise lab 01 - 15/Seven_PPT_2.py
+++ Grade 07/Exercise lab 01 - 15/Seven_PPT_2.py
@@ -4,11 +4,18 @@ Ex 2.
 Create 2 variables, time1 and time2 and print the greater variable
 
 Hint: Use if(): and else:
+Program description: The objective of this program is to print the greatest value of the particular data type of both the variables.
+In this program we will be declaring two variables” time1” and “time2”and will initialize integer value to both the variables ie:10 and 15 respectively. 
+And using the looping statement ifelse we will be checking if value of time1 is greater than time 2 so the condition is if time1 is greater than time2
+then print time1 is greater if the condition is not satisfied then we move to the else statement and print that statement
+NOTE: FOR COMPARISION BETWEEN TWO VARIABLES THE DATA TYPE OF BOTH THE VALUES OF THE INTEAGER SHOULD BE HOMOGENEOUS.
+
+
 """
 
-time1 = 10
-time2 = 15
-if(time1 > time2):
-  print("time1")
+vAR_time1 = 10
+vAR_time2 = 15
+if(vAR_time1 > vAR_time2):
+  print("vAR_time1")
 else:
-  print("time2")
+  print("vAR_time2")

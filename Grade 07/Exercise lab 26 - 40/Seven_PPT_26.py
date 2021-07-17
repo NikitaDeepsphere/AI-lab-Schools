e038a153473f30af5cb9a2802fb56820a3d3556b
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-07-17 17:21:05 +0530

    Update Seven_PPT_26.py

--- Grade 07/Exercise lab 26 - 40/Seven_PPT_26.py
+++ Grade 07/Exercise lab 26 - 40/Seven_PPT_26.py
@@ -2,6 +2,11 @@
 Ex 26.
 
 Create a class titled animal that has a variable animal_type.
+
+Program description: The objective of this program is to create a class titled animal which has a variable animal_type. 
+This program highlights the usage of class in python. A class is a code template for creating objects. 
+Note: In python a class is created by using a keyword name “class”
+
 """
 
 class animal:

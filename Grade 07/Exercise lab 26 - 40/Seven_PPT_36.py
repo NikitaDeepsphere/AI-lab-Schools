f739c395f28c2324871c4f58a3a9e20d902c25d7
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-07-17 18:04:18 +0530

    Update Seven_PPT_36.py

--- Grade 07/Exercise lab 26 - 40/Seven_PPT_36.py
+++ Grade 07/Exercise lab 26 - 40/Seven_PPT_36.py
@@ -3,33 +3,11 @@ Ex 36.
 
 Given the class animal (see below), create a new animal dog, and name it Fido with it's gender as Male and age as 6.
 
- class animal:
-   animal_name = ""
-   age = 0
-   gender = ""
+Program description: The objective of this program is to add a function to the Calculator Class which will call one of the other functions and return the output. 
+It uses a set of if and else statements to test the operation attribute against “*”,”/”, “+”, “-“ and decide which operation to use.
+Additionally, a line to test out the Calculator Class functions is also provided.
+
  
-   def __init__ (self, animal_name, animal_age, animal_gender):
-     self.animal_name = animal_name
-     self.age = int(animal_age)
-     self.gender = animal_gender
- 
-   def change_name(self,str_in):
-     self.animal_name = str_in
- 
-   def change_age(self,int_in):
-      self.age = int(int_in)
- 
-   def change_gender(self,str_in):
-      self.gender = str_in 
- 
-   def get_age(self):
-     return self.age
- 
-   def get_name(self):
-     return self.animal_name
- 
-   def get_gender(self):
-     return self.gender
 """
 
 class animal:

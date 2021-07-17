4384c636018c61742d1831f181c2fa9dabec0f7e
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-07-17 17:53:58 +0530

    Update Seven_PPT_33.py

--- Grade 07/Exercise lab 26 - 40/Seven_PPT_33.py
+++ Grade 07/Exercise lab 26 - 40/Seven_PPT_33.py
@@ -2,6 +2,9 @@
 Ex 33.
 
 Add to the Calculator class a function that will add the 2 numbers.
+
+Program description: The objective of this program is to add a function to the Calculator Class that will add 2 numbers.
+Here the Class has 3 attributes that are edited in the first function and the final answer is returned in the second function.
 """
 
 class Calculator:

96050d6840985e3de5d3c687218f572d8a3b4b67
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-07-17 13:53:20 +0530

    Update Seven_PPT_13.py

--- Grade 07/Exercise lab 01 - 15/Seven_PPT_13.py
+++ Grade 07/Exercise lab 01 - 15/Seven_PPT_13.py
@@ -2,11 +2,17 @@
 Ex 13.
 
 Given a list of integer numbers, return the amount of numbers in that list that are even.
+
+Program description: def is the keyword for defining a function. The function name is followed by parameters in (). 
+The colon signals the start of the function body, which is marked by indentation. 
+Inside the function body, the return statement determines the value to be returned. 
+Count =0 we will check if mod of 2, x in numbers list ==0 then we will increment count by 1.
 """
-numbers = [1,2,3,4,5,6,7,8] # can be any list, this is just an example
-def count_evens(numbers):
-  count = 0
-  for x in numbers:
-    if x % 2 == 0:
-      count+=1
-  return count
+
+vAR_numbers = [1,2,3,4,5,6,7,8] # can be any list, this is just an example
+def vAR_count_evens(vAR_numbers):
+  vAR_count = 0
+  for vAR_x in vAR_numbers:
+    if vAR_x % 2 == 0:
+      vAR_count+=1
+  return vAR_count

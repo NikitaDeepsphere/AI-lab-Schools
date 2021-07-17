5048af05c5b9d5fa01f843b492756f615b35645a
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-07-17 17:22:57 +0530

    Update Seven_PPT_27.py

--- Grade 07/Exercise lab 26 - 40/Seven_PPT_27.py
+++ Grade 07/Exercise lab 26 - 40/Seven_PPT_27.py
@@ -4,10 +4,15 @@ Ex 27.
 Add to the preivous class and create a function change_type that changes the type of the animal based on what is passed in.
 
 Hint: remember to use self as one of the parameters passed into the function
+
+Program description: The objective of this program is to add to the previous class and also create a function 
+“change_type” which changes the type of the animal based on what is passed in.
+Note : “def” is used to create a function whereas “class” is used to create a class.
+
 """
 
 class animal:
    animal_type = ""
 
-   def change_type(self,str_in):
-     self.animal_type = str_in
+   def change_type(self,vAR_str_in):
+     self.animal_type = vAR_str_in

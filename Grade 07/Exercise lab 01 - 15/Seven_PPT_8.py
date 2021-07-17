21ac4880f1abec1426b82ae587fa4d32274a91ab
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-07-17 13:39:09 +0530

    Update Seven_PPT_8.py

--- Grade 07/Exercise lab 01 - 15/Seven_PPT_8.py
+++ Grade 07/Exercise lab 01 - 15/Seven_PPT_8.py
@@ -2,9 +2,10 @@
 Ex 8.
 
 Using the list created in ex. 6, given a new list, new_fruits = ["Pineapple", "Coconut", "Watermelon"], combine the two.
-"""
 
-fruits = ["Bannana", "Apple", "Peach"]
-new_fruits = ["Pineapple", "Coconut", "Watermelon"]
-for x in new_fruits:
-  fruits.append(x)
+Program description: here we are going to combine list 1 with list 2we use append function in this case.
+"""
+vAR_fruits = ["Bannana", "Apple", "Peach"]
+vAR_new_fruits = ["Pineapple", "Coconut", "Watermelon"]
+for vAR_x in vAR_new_fruits:
+  vAR_fruits.append(vAR_x)

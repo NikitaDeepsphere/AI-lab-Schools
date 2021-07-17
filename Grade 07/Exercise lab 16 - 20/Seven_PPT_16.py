1e3a1c996566cb2bfbf77b491ee5a8460be03cb2
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-07-17 14:06:16 +0530

    Update Seven_PPT_16.py

--- Grade 07/Exercise lab 16 - 20/Seven_PPT_16.py
+++ Grade 07/Exercise lab 16 - 20/Seven_PPT_16.py
@@ -4,14 +4,14 @@ Ex 16.
 Create a function that will check whether the sum of a list that is passed in equal to 21
 """
 
-check = [1,2,-3,3,3,4,5,6,2,-2]
-def sum21(list):
-  total = 0
-  for x in check:
-    total+=x
-  if(total == 21):
+vAR_check = [1,2,-3,3,3,4,5,6,2,-2]
+def vAR_sum21(list):
+  vAR_total = 0
+  for vAR_x in vAR_check:
+    vAR_total+=vAR_x
+  if(vAR_total == 21):
     return True
   else:
     return False
 
-print(sum21(check))
+print(vAR_sum21(vAR_check))

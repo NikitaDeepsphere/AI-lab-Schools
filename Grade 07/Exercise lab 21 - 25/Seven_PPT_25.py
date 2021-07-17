9b9d2a224ba79464df97829a61e4a0c0ff8d5c29
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-07-17 17:18:37 +0530

    Update Seven_PPT_25.py

--- Grade 07/Exercise lab 21 - 25/Seven_PPT_25.py
+++ Grade 07/Exercise lab 21 - 25/Seven_PPT_25.py
@@ -4,11 +4,14 @@ Ex 25.
 Create a function that will find the index of where the first time the character 'e' occurs in a string.
 
 Hint: Use the function .index('e')
+
+Program description: The objective of this program is to create a function that 
+will find the index of where the first time the character ‘e’ occurs in a string.
 """
 
-str = "Where is there an e here?"
+vAR_str = "Where is there an e here?"
 
 def indexOf(string):
-  return str.index('e')
+  return vAR_str.index('e')
 
-print(indexOf(str))
+print(indexOf(vAR_str))

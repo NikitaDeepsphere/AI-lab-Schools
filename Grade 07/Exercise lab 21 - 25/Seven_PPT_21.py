7d7c02bc1989b071a80d886e5767bd4bb1886693
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-07-17 17:06:53 +0530

    Update Seven_PPT_21.py

--- Grade 07/Exercise lab 21 - 25/Seven_PPT_21.py
+++ Grade 07/Exercise lab 21 - 25/Seven_PPT_21.py
@@ -2,14 +2,21 @@
 Ex 21.
 
 Create a function that will take in a string and output the string but with each character doubled. For example: double("Apple") = "AAppppllee"
+
+Program description: The objective of this program is to create a function that will take output the string but with each character doubled.
+For example:
+                    *(“Apple”) = AAppppllee
+                    *(“Pear”) = PPeeaarr
+Note : The each letter is doubled
+
 """
 
-str1 = "Apple"
+vAR_str1 = "Apple"
 
-def double(string):
-  output = ""
-  for x in range(len(string)):
-    output = output + string[x] + string[x]
-  return output
+def double(vAR_string):
+  vAR_output = ""
+  for vAR_x in range(len(vAR_string)):
+    vAR_output = vAR_output + vAR_string[vAR_x] + vAR_string[vAR_x]
+  return vAR_output
 
-print(double(str1))
+print(double(vAR_str1))

92aa88f3e3e0a87f85bb1a01df46e39ab443191d
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-07-17 17:57:35 +0530

    Update Seven_PPT_34.py

--- Grade 07/Exercise lab 26 - 40/Seven_PPT_34.py
+++ Grade 07/Exercise lab 26 - 40/Seven_PPT_34.py
@@ -2,26 +2,31 @@
 Ex 34.
 
 Add to the Calculator class a function that will add the subtract the 2 numbers, one that will divide, and one that will multiply
+
+Program description: The objective of this program is to add 3 functions to the Calculator Class that will subtract, 
+multiply, and divide apart from the already existing add function (Ex  33). 
+Note: Doesn’t the Calculator Class now resemble a 4-function Calculator?
+
 """
 
 class Calculator:
-   num1 = 0
-   num2 = 0
-   operation = ""
+   vAR_num1 = 0
+   vAR_num2 = 0
+   vAR_operation = ""
 
-   def __init__ (self, num1, num2, operation):
-     self.num1 = int(num1)
-     self.num2 = int(num2)
-     self.operation = operation
+   def __init__ (self, vAR_num1, vAR_num2, vAR_operation):
+     self.vAR_num1 = int(vAR_num1)
+     self.vAR_num2 = int(vAR_num2)
+     self.vAR_operation = vAR_operation
 
    def add(self):
-     return self.num1+self.num2
+     return self.vAR_num1+self.vAR_num2
 
    def subtract(self):
-     return self.num1-self.num2
+     return self.vAR_num1-self.vAR_num2
 
    def multiply(self):
-     return self.num1*self.num2
+     return self.vAR_num1*self.vAR_num2
 
    def divide(self):
-     return self.num1/self.num2
+     return self.vAR_num1/self.vAR_num2

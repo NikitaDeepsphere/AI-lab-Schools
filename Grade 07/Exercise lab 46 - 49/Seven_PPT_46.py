aec881e67bf8ada60af05a4c7cee8ace4a4d73ec
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-07-18 11:59:06 +0530

    Update Seven_PPT_46.py

--- Grade 07/Exercise lab 46 - 49/Seven_PPT_46.py
+++ Grade 07/Exercise lab 46 - 49/Seven_PPT_46.py
@@ -1,7 +1,13 @@
 """
 Ex 46.
 
-Create a class called bank_account that has the variables balance and id_number. The constructor will take in a number for the balance and the id_number will be a random 6-digit number.
+Create a class called bank_account that has the variables balance and id_number. 
+The constructor will take in a number for the balance and the id_number will be a random 6-digit number.
+
+Program description: The objective of this code is to create a class called bank_account that has the variables balance and id_number.
+The constructor will take in a number for the balance and the id_number will be a random 6-digit number. 
+Note: Use the random in-built function to generate the 6-digit number.
+
 """
 
 import random

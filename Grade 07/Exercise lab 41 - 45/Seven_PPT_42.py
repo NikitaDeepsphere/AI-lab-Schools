1f13e40cea1a20ef37ed9c873c714fd3cbd26934
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-07-18 11:54:34 +0530

    Update Seven_PPT_42.py

--- Grade 07/Exercise lab 41 - 45/Seven_PPT_42.py
+++ Grade 07/Exercise lab 41 - 45/Seven_PPT_42.py
@@ -1,7 +1,19 @@
 """
 Ex 42.
 
-Create a class classroom. create the variables class_size, student_list,average_age, and id_range. Give them default values. Create the constructor which takes in a number for the classroom size and uses that to create a list of students with the name "Mark", a random 6- digit id number and either the age 14 or 15.
+Create a class classroom. create the variables class_size, student_list,average_age, and id_range. Give them default values. 
+Create the constructor which takes in a number for the classroom size and uses that to create a list of students with the name "Mark",
+a random 6- digit id number and either the age 14 or 15.
+
+
+Program description: The objective of this code is to create a Class Classroom with the following characteristics:
+-	Variables: class_size, student_list,average_age, and id_range
+-	Give them default values. 
+-	Create a constructor which takes in a number for the classroom size and uses that to create a list of students with the name "Mark", a random 6- digit id number and either the age 14 or 15.
+
+Note: Use a loop for the last charcateristics and the random in-built function
+
+
 """
 
 import random

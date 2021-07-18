959939448b807bb27b7988b6d30045ce4d3cf337
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-07-18 12:00:54 +0530

    Update Seven_PPT_47.py

--- Grade 07/Exercise lab 46 - 49/Seven_PPT_47.py
+++ Grade 07/Exercise lab 46 - 49/Seven_PPT_47.py
@@ -2,6 +2,11 @@
 Ex 47.
 
 Add to the bank_account class 2 functions, withdraw and despoit, which will take in a integer input and add/subtract the the balance the corresponding amount.
+
+Program description: The objective of this code is the create 2 new functions to the bank_account class.
+The first one is called withdraw which will take in an integer input and subtract it from the balance. 
+The second is called deposit and will again take in an integer input and add it to the existing balance.
+Thus, they change the values of the balance attributes.
 """
 
 import random

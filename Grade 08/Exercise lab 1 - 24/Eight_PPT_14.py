9665dc9d43e1cdc10b090b02eeccf0843537c00a
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-07-31 14:09:12 -0700

    Update Eight_PPT_14.py

--- Grade 08/Exercise lab 1 - 24/Eight_PPT_14.py
+++ Grade 08/Exercise lab 1 - 24/Eight_PPT_14.py
@@ -1,15 +1,15 @@
 """
 Ex 14.
 
-Create a function that will check whether the sum of a list that is passed in equal to 21
+The objective of this program is to create a function that will check whether the sum of a list that is imputed is equal to 21.
 """
 
 check = [1,2,-3,3,3,4,5,6,2,-2]
 def sum21(list):
-  total = 0
-  for x in check:
-    total+=x
-  if(total == 21):
+  vAR_total = 0
+  for vAR_x in check:
+    vAR_total+=vAR_x
+  if(vAR_total == 21):
     return True
   else:
     return False

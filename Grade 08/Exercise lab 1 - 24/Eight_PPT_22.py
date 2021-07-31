8602af54c0128c5640b9c36d355f4da9218941fa
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-07-31 14:49:24 -0700

    Update Eight_PPT_22.py

--- Grade 08/Exercise lab 1 - 24/Eight_PPT_22.py
+++ Grade 08/Exercise lab 1 - 24/Eight_PPT_22.py
@@ -1,23 +1,25 @@
 """
 Ex 22.
 
-Add to the bank_account class 2 functions, withdraw and despoit, which will take in a integer input and add/subtract the the balance the corresponding amount.
+Program Description: The objective of this program is to create two functions that will allow the user to withdraw or deposit money into the bank account (from the class in the previous program.)
+
+Note: The input will be taken as an integer
 """
 
 import random
 
 class bank_account:
-  balance = 0
-  id_number = 0
+  vAR_balance = 0
+  vAR_id_number = 0
 
-  def __init__(self, num):
-    self.balance = num
+  def __init__(self, vAR_num):
+    self.balance = vAR_num
     self.id_number = random.randrange(100000,999999)
 
-  def withdraw(self, num):
-    self.balance = self.balance - num
+  def withdraw(self, vAR_num):
+    self.balance = self.balance - vAR_num
     return self.balance
   
-  def deposit(self, num):
-    self.balance = self.balance + num
+  def deposit(self, vAR_num):
+    self.balance = self.balance + vAR_num
     return self.balance

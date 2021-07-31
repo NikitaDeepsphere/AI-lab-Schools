3953a4b20091681c750f915ac1930d60b7aac2d5
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-07-31 14:06:34 -0700

    Update Eight_PPT_13.py

--- Grade 08/Exercise lab 1 - 24/Eight_PPT_13.py
+++ Grade 08/Exercise lab 1 - 24/Eight_PPT_13.py
@@ -1,15 +1,15 @@
 """
 Ex 13.
 
-Given a list of integer numbers, find the sum of all numbers that are either odd or less than 100
+The objective of this program is to create a function that can print the sum of all integers that are odd or less than 100.
 """
 
 numbers = [12,145,14,122,142,2,3154,132,132,242,1245,235,123] # can be any list, this is just an example
-def odd_or_hundred(numbers):
-  total=0
-  for x in numbers:
-    if(x%2 == 1 or x<100):
-      total+=x
-  return total
+def odd_or_hundred(vAR_numbers):
+  vAR_total=0
+  for vAR_x in vAR_numbers:
+    if(vAR_x%2 == 1 or vAR_x<100):
+      vAR_total+=vAR_x
+  return vAR_total
 
 print(odd_or_hundred(numbers))

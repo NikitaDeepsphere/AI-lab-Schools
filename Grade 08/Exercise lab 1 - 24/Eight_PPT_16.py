d277a9a4daeedd5e2fb15d2500fc7aefefe466ae
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-07-31 14:13:54 -0700

    Update Eight_PPT_16.py

--- Grade 08/Exercise lab 1 - 24/Eight_PPT_16.py
+++ Grade 08/Exercise lab 1 - 24/Eight_PPT_16.py
@@ -1,7 +1,7 @@
 """
 Ex 16.
 
-The objective of this program is to create a function that will find the index of where the first time character ‘e’ appears in a string. 
+Program Description: The objective of this program is to create a function that will find the index of where the first time character ‘e’ appears in a string. 
 
 Note- Use the function .index(‘e’) for successful execution of the program
 """

92920f992fd526dd55f468c589695aa89ccea10a
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-08-05 15:49:01 +0400

    Update Eight_PPT_10.py

--- Grade 08/Exercise lab 1 - 24/Eight_PPT_10.py
+++ Grade 08/Exercise lab 1 - 24/Eight_PPT_10.py
@@ -2,6 +2,10 @@
 Ex 10.
 
 Given a list of integer numbers, find the range of the list (the maximum - minimum value)
+
+Program Description: The objective of this program is to create a function that can print the range(Maximum- minimum list) of integers inputted by the user.
+Note- Range is the difference between the highest and lowest values
+
 """
 
 numbers = [12,145,14,122,142,2,3154,132,132,242,1245,235,123] # can be any list, this is just an example

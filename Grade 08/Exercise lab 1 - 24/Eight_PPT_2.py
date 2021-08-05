79e1c820e71f5c48d4b2d2d7dfa5417a9a5e41e2
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-08-05 15:17:24 +0400

    Update Eight_PPT_2.py

--- Grade 08/Exercise lab 1 - 24/Eight_PPT_2.py
+++ Grade 08/Exercise lab 1 - 24/Eight_PPT_2.py
@@ -3,7 +3,11 @@ Ex 2.
 
 Write a for loop to print out the counting numbers from 0 - 9
 
-Hint: for x in ___:
+Program description : The objective of this program is to print counting numbers 0-9 using for loop.
+So, for i in range(10) signifies range between 0-9.
+
+Note: To execute the same output we can also use other conditional statements such as “Downwhile” and “While”.
+
 """
 
 for x in range(10):

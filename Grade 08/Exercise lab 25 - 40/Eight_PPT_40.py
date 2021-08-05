fa234a1ad358feac8e57b6fba48fbb21789bc676
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-08-05 15:12:12 +0400

    Update Eight_PPT_40.py

--- Grade 08/Exercise lab 25 - 40/Eight_PPT_40.py
+++ Grade 08/Exercise lab 25 - 40/Eight_PPT_40.py
@@ -2,6 +2,10 @@
 Ex 40.
 
 Create and show a pie chart that has 4 randomized sections.
+
+Program Description: The objective of this program is to create a pie chart that is randomly divided into 4 sections by entering the percentage out of 100 with the random function.
+Note: You need to add a range using random.randrange(start, end)
+
 """
 
 import matplotlib.pyplot as plt

fcb7cf48ea93de1cceb77934dc8de7bed75ba768
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-08-05 15:45:11 +0400

    Update Eight_PPT_7.py

--- Grade 08/Exercise lab 1 - 24/Eight_PPT_7.py
+++ Grade 08/Exercise lab 1 - 24/Eight_PPT_7.py
@@ -2,6 +2,9 @@
 Ex 7.
 
 Say an apple costs .59 cents and a pear costs .99 cents. Using user input and ommiting any taxes, find out how much it costs if one buys 3 apples and 4 pears.
+
+Program description : The objective of this program is to find out how much it costs if one buys 3 apples and 4 pears. If one apple costs 0.59 cents and one pear costs 0.99 cents. It will calculate the total amount.
+
 """
 
 apples  = int(input("How many apples:"))

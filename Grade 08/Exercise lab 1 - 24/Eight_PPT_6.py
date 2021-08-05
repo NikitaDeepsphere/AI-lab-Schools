dd0153f225709937f199b4249b29c82db20e76ba
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-08-05 15:44:27 +0400

    Update Eight_PPT_6.py

--- Grade 08/Exercise lab 1 - 24/Eight_PPT_6.py
+++ Grade 08/Exercise lab 1 - 24/Eight_PPT_6.py
@@ -2,6 +2,10 @@
 Ex 6.
 
 Grab the username and password through user input using input(). Print back the result.
+
+Program description : The objective of this program is to grab username and password through user input using input() and print back the result.
+Note :This program highlights the usage of input() function in Python.
+
 """
 
 username = input("Enter username:")

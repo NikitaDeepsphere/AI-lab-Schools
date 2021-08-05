cce673a2dd2402729661bc0d510a447ee539f9fe
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-08-05 14:52:39 +0400

    Update Eight_PPT_47.py

--- Grade 08/Exercise lab 41 - 47/Eight_PPT_47.py
+++ Grade 08/Exercise lab 41 - 47/Eight_PPT_47.py
@@ -2,6 +2,10 @@
 Ex 47.
 
 Using .arange, create an array with the counting numbers from 0 to 19.
+
+Program Description: The objective of this program is to create an array counting numbers upto a specified values. 
+Note: Use the format np.arrange(x) where x is a number.
+
 """
 
 import numpy as np

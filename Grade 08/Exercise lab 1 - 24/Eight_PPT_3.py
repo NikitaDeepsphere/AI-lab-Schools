5fbcd39ebfddaf2fca8f97aa78f107c1fa154582
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-08-05 15:19:55 +0400

    Update Eight_PPT_3.py

--- Grade 08/Exercise lab 1 - 24/Eight_PPT_3.py
+++ Grade 08/Exercise lab 1 - 24/Eight_PPT_3.py
@@ -3,6 +3,10 @@ Ex 3.
 
 Write a loop to print out counting numbers from 0 - 9, but use ```while``` instead of for
 
+Program description : The objective of this program is to print the numbers in range using a while loop. Here we declare a variable “x” and initialize it as 0 and give it with a while condition that the value should be lesser than 10.and we also increment which is increasing the value of x by 1, each time the loop runs. And we print so each time the loop runs it will increase its value by 1 and check with the condition and if the condition satisfies its proceeds or if not, the loop terminates.
+Note: The value can also decrement by using the condition “x=x-1”. But for decrementing we have to initialize the value of x as 10.
+Try it out : Try printing 0-9 using a while loop by decrementing. 
+
 """
 
 x = 0

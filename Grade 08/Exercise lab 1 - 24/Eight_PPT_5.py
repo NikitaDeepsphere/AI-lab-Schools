7becc4bf61b639c6b05b864b89f33492535da179
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-08-05 15:43:58 +0400

    Update Eight_PPT_5.py

--- Grade 08/Exercise lab 1 - 24/Eight_PPT_5.py
+++ Grade 08/Exercise lab 1 - 24/Eight_PPT_5.py
@@ -2,6 +2,9 @@
 Ex 5.
 
 Using the list created in ex. 4, given the list, new_fruits = ["Pineapple", "Watermelon"], replace the corresponding value of fruits, with the corresponding values of new_fruits
+
+Program description : The objective of this program is using the list created in exercise -4 (fruits) Create another list named new_fruits which contains values Pineapple and watermelon and to replace the corresponding values of fruits with corresponding values of new_fruits.
+
 """
 
 fruits = ["Bannana", "Apple", "Peach"]

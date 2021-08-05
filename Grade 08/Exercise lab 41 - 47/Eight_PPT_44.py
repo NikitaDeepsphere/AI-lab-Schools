85f689ac5d621bb987fbb21b18aa646d3d87f629
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-08-05 14:54:53 +0400

    Update Eight_PPT_44.py

--- Grade 08/Exercise lab 41 - 47/Eight_PPT_44.py
+++ Grade 08/Exercise lab 41 - 47/Eight_PPT_44.py
@@ -2,6 +2,10 @@
 Ex 44.
 
 Given the arrays, arr = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]) and arr1 = np.array([2, 3, 4]), find the common items present in both arrays and delete them from the arr.
+
+Program Description: The objective of this program is to find and delete the common items present in two arrays. These common items are put into a new set. 
+Note: Use the format np.setdiff1d(arr1, arr2)
+
 """
 
 import numpy as np

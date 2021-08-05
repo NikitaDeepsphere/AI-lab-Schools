bfb27bff09454abe1da4088029ab07580fcf0d10
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-08-05 15:07:55 +0400

    Update Eight_PPT_39.py

--- Grade 08/Exercise lab 25 - 40/Eight_PPT_39.py
+++ Grade 08/Exercise lab 25 - 40/Eight_PPT_39.py
@@ -2,6 +2,9 @@
 Ex 39.
 
 Create and show a pie chart that has 4 equal sections.
+
+Program Description: The objective of this program is to create a pie chart that is equally divided into 4 sections by entering the percentage out of 100 for each part. 
+
 """
 
 import matplotlib.pyplot as plt

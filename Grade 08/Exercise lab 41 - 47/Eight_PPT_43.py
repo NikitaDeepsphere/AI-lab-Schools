644e75377452b3fc1fafcee950cd3fbc4cdb5013
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-08-05 14:55:43 +0400

    Update Eight_PPT_43.py

--- Grade 08/Exercise lab 41 - 47/Eight_PPT_43.py
+++ Grade 08/Exercise lab 41 - 47/Eight_PPT_43.py
@@ -2,6 +2,10 @@
 Ex 43.
 
 Given the arrays, arr = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]) and arr1 = np.array([2, 3, 4]), find the common items present in both arrays.
+
+Program Description: The objective of this program is to find the common items between two arrays using numpy. The common items get added to a new array.
+Note: Use the format np.intersec1d(arr1, arr2) to find the common items.
+
 """
 
 import numpy as np

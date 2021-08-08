ed1bbbc69ed9abf304b519e06d341afa091743bf
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-08-08 15:19:29 -0700

    Update Eight_PPT_27.py

--- Grade 08/Exercise lab 25 - 40/Eight_PPT_27.py
+++ Grade 08/Exercise lab 25 - 40/Eight_PPT_27.py
@@ -1,7 +1,8 @@
 """
 Ex 27.
-
 Plot the parabola y = x^2 + 6x + 9 from x = -10 to x = 10
+
+Program Description: The objective of this program is to plot the equation of the parabola (y = x^2 + 6x + 9) from the x values -10 and 10.
 """
 import matplotlib.pyplot as plt
 import numpy as np

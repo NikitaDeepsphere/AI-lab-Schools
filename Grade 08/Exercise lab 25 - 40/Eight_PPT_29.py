b57fae0b72aa094b2ae12ab08aec2d4ab897cd4d
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-08-08 15:28:27 -0700

    Update Eight_PPT_29.py

--- Grade 08/Exercise lab 25 - 40/Eight_PPT_29.py
+++ Grade 08/Exercise lab 25 - 40/Eight_PPT_29.py
@@ -1,9 +1,10 @@
 """
 Ex 29.
-
 Plot this data with lines connecting each of the points.
+ex- (Student, GPA) --> (1, 3.0) ; (2, 4.0) ; (3, 2.5) ; (4, 3.67) ; (5, 2.0)
 
-(Student, GPA) --> (1, 3.0) ; (2, 4.0) ; (3, 2.5) ; (4, 3.67) ; (5, 2.0)
+Program description : The objective of this program is to plot this data with lines connecting each of the points.
+
 """
 import matplotlib.pyplot as plt
 import numpy as np

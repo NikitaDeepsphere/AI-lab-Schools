2129d6384db110915422dd68a243b4706e049e07
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-09-05 11:09:03 -0700

    Update Nine_PPT_4.py

--- Grade 09/Exercise lab 1 - 10/Nine_PPT_4.py
+++ Grade 09/Exercise lab 1 - 10/Nine_PPT_4.py
@@ -2,6 +2,8 @@
 Ex 4.
 
 Program description: The objective of this program is to create the variables “year” , “month” and “day”.
+
+Note: This part of code is being added to the previously created code in Exercise - 2.
 """
 
 days = ["Sunday", "Monday", "Tuesday", "Wensday", "Thursday", "Friday", "Saturday"]

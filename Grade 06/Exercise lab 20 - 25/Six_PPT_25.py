a149864eeda56aed2b5bd39ae2d7e37f00d2856a
Nikita <101127072+NikitaDeepsphere@users.noreply.github.com>
2022-10-25 21:29:14 +0530

    Update Six_PPT_25.py

--- Grade 06/Exercise lab 20 - 25/Six_PPT_25.py
+++ Grade 06/Exercise lab 20 - 25/Six_PPT_25.py
@@ -8,21 +8,21 @@ Program description: The objective of this program is to add all the values of a
 
 vARint_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-for x in range(len(vARint_list)):
-  vARint_list[x]*=12
+for vAR_x in range(len(vARint_list)):
+  vARint_list[vAR_x]*=12
 
 vAR_add = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30]
 for vAR_x in range(len(vARint_list)):
-  vARint_list[x] = (vARint_list[x] + vAR_add[x])
+  vARint_list[vAR_x] = (vARint_list[vAR_x] + vAR_add[vAR_x])
 
 vAR_total = 0
-for x in range (len(vARint_list)):
-  vAR_total+= vARint_list[x]
+for vAR_x in range (len(vARint_list)):
+  vAR_total+= vARint_list[vAR_x]
 
-for x in range(len(vARint_list)):
-  vARint_list[x] = str(vARint_list[x])
+for vAR_x in range(len(vARint_list)):
+  vARint_list[vAR_x] = str(vARint_list[vAR_x])
 
 print_me = ""
-for x in range(len(vARint_list)):
-  print_me = print_me + " " + vARint_list[x]
+for vAR_x in range(len(vARint_list)):
+  print_me = print_me + " " + vARint_list[vAR_x]
 print(print_me)

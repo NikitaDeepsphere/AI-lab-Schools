a44f1400e22ae818d8de64bf30510e7267104657
Nikita <101127072+NikitaDeepsphere@users.noreply.github.com>
2022-10-25 21:27:17 +0530

    Update Six_PPT_24.py

--- Grade 06/Exercise lab 20 - 25/Six_PPT_24.py
+++ Grade 06/Exercise lab 20 - 25/Six_PPT_24.py
@@ -8,16 +8,16 @@ Program description: The objective of this program is to change the data type of
 
 vARint_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-for x in range(len(vARint_list)):
-  vARint_list[x]*=12
+for vAR_x in range(len(vARint_list)):
+  vARint_list[vAR_x]*=12
 
 vAR_add = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30]
-for x in range(len(vARint_list)):
-  vARint_list[x] = (vARint_list[x] + vAR_add[x])
+for vAR_x in range(len(vARint_list)):
+  vARint_list[vAR_x] = (vARint_list[vAR_x] + vAR_add[vAR_x])
 
 vAR_total = 0
-for x in range (len(vARint_list)):
-  vAR_total+= vARint_list[x]
+for vAR_x in range (len(vARint_list)):
+  vAR_total+= vARint_list[vAR_x]
 
-for x in range(len(vARint_list)):
-  vARint_list[x] = str(vARint_list[x])
+for vAR_x in range(len(vARint_list)):
+  vARint_list[vAR_x] = str(vARint_list[vAR_x])
